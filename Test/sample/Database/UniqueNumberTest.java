03dc6b18297b64a0475e7ca408b536e52765eb55
alecv <Alecvanspr@gmail.com>
2021-06-02 17:25:35 +0200

    foto+ files die nodig zijn voor de database en de test ervan

---
+++ Test/sample/Database/UniqueNumberTest.java
@@ -0,0 +1,45 @@
+package sample.Database;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/** 
+* UniqueNumber Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>mei 28, 2021</pre> 
+* @version 1.0 
+*/ 
+public class UniqueNumberTest {
+/** 
+* 
+* Method: getUniqueNumber(String path) 
+* 
+*/ 
+@Test
+public void testGetUniqueNumber() throws Exception {
+    Assert.assertEquals("1",UniqueNumber.getUniqueNumber("src/db/MaxTest.txt"));
+    clearTestFile();
+}
+
+/** 
+* 
+* Method: getNewUniqueNumber(String path) 
+* 
+*/ 
+@Test
+public void testGetNewUniqueNumber() throws Exception {
+    Assert.assertEquals(1,UniqueNumber.getNewUniqueNumber("src/db/MaxTest.txt"));
+    Assert.assertEquals(2,UniqueNumber.getNewUniqueNumber("src/db/MaxTest.txt"));
+    Assert.assertEquals(3,UniqueNumber.getNewUniqueNumber("src/db/MaxTest.txt"));
+    clearTestFile();
+}
+public void clearTestFile(){
+    FileUpdater fileUpdater = new FileUpdater();
+    ArrayList<String> arrayList = new ArrayList();
+    arrayList.add("1");
+    fileUpdater.updateFile("src/db/MaxTest.txt",arrayList);
+}
+}
