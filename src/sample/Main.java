eb70f99ad57ad65d5a4c242ea9488828cff737dd
alecv <Alecvanspr@gmail.com>
2021-06-11 15:32:51 +0200

    main zat nog wat overbodige code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -29,17 +29,4 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    public Date maakDate(String datum){
-        //Deze maakt de substrings
-        System.out.println(datum);
-        int day = Integer.parseInt(datum.substring(0,2));
-        System.out.println(datum);
-        int month = Integer.parseInt(datum.substring(3,5));
-        System.out.println(datum);
-        int year = Integer.parseInt(datum.substring(6,10));
-        System.out.println(datum);
-        //Deze maakt de Datum die uiteindelijk gereturned wordt.
-        Date date = new GregorianCalendar(year,month-1,day).getTime();
-        return date;
-    }
 }
